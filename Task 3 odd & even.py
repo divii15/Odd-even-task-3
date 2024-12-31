b07d2b2f0f2050470c94c34bcbfb6343f9e85f1e
divii15 <divyadaksha143@gmail.com>
2024-12-31 10:55:42 +0530

    My next task(3)(2) 31/12

---
+++ Task 3 odd & even.py
@@ -0,0 +1,37 @@
+a = 1,2,3,4,5,6,7,8
+d1 = 0
+d2 = 0
+if a[0]%2==0:
+    d1 += 1
+else:
+    d2 += 1
+if a[1]%2==0:
+    d1 += 1
+else:
+    d2 += 1
+if a[2]%2==0:
+    d1 += 1
+else:
+    d2 += 1
+if a[3]%2==0:
+    d1 += 1
+else:
+    d2 += 1
+if a[4]%2==0:
+    d1 += 1
+else:
+    d2 += 1
+if a[5]%2==0:
+    d1 += 1
+else:
+    d2 += 1
+if a[6]%2==0:
+    d1 += 1
+else:
+    d2 += 1
+if a[7]%2==0:
+    d1 += 1
+else:
+    d2 += 1
+print("The even number in the list are:", d1)
+print("The odd number in the list are:", d2)
